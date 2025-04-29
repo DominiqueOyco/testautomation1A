e5801ed22712c3b4dc0b92d59622061ab5b463bb
DominiqueOyco <dominickoyco@gmail.com>
2025-04-28 20:29:50 -0700

    Updated code 1

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,7 +6,7 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('login page', () => {
     test('Verify user is able to input username in the username field', async ({ page }) => {
-        await page.getByPlaceholder('Username').fill('standard-user')
+        await page.getByPlaceholder('Username').fill('standard_user')
     })
 
     test('Verify user is able to enter a hidden password in the password field', async ({ page }) => {
@@ -14,14 +14,19 @@ test.describe('login page', () => {
     })
 
     test('Verify Login CTA transfers user to the products page when the username and password are valid', async ({ page }) => {
-        await page.getByPlaceholder('Username').fill('standard-user')
+        await page.getByPlaceholder('Username').fill('standard_user')
         await page.getByPlaceholder('Password').fill('secret_sauce')
         await page.getByRole('button').click();
-
+        await page.waitForTimeout(500)
+        await expect(page.url()).toBe('https://www.saucedemo.com/v1/inventory.html')
     })
 
     test('Verify Login CTA does not transfer user to the main page when the username and password are invalid', async ({ page }) => {
-
+        await page.getByPlaceholder('Username').fill('aaaaa')
+        await page.getByPlaceholder('Password').fill('aaaaa')
+        await page.getByRole('button').click();
+        await page.waitForTimeout(500)
+        await expect(page.url()).toBe('https://www.saucedemo.com/v1/index.html')
     })
 
     test('Verify invalid credentials produces an error: Epic sadface: Username and password do not match any user in this service', async ({ page }) => {
